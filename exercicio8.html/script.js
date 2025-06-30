b3906376960439d224774c665ee51211fff15263
leandrobastos07 <titoele12@gmail.com>
2025-06-29 22:21:53 -0300

    Add files via upload

--- exercicio8.html/script.js
+++ exercicio8.html/script.js
@@ -5,24 +5,24 @@ let mostraResultado = document.querySelector(`#resultado`);
 estacao = window.prompt("Digite o mês: ").toUpperCase()
 
 if (estacao == "JANEIRO" || estacao == "FEVEREIRO" || estacao == "DEZEMBRO") { 
-    mostraresultado.innerHTML = `<p>A estação é: Verão</p>`;
-    document.body.style.backgroundColor = "blue";
+    mostraResultado.innerHTML = `<p>A estação é: Verão</p>`;
+    document.body.style.backgroundColor = "red";
 
 }
 
 if (estacao == "MARÇO" || estacao == "ABRIL" || estacao == "MAIO") {
-    mostraresultado.innerHTML = `<p>A estação é: Outono</p>`;
-    document.body.style.backgroundColor = "blue";
+    mostraResultado.innerHTML = `<p>A estação é: Outono</p>`;
+    document.body.style.backgroundColor = "orange";
 
 }
 if (estacao == "JUNHO" || estacao == "JULHO" || estacao == "AGOSTO") {
-    mostraresultado.innerHTML = `<p>A estação é: Inverno</p>`;
+    mostraResultado.innerHTML = `<p>A estação é: Inverno</p>`;
     document.body.style.backgroundColor = "blue";
 
 }
 if (estacao == "SETEMBRO" || estacao == "OUTUBRO" || estacao == "NOVEMBRO") {
-    mostraresultado.innerHTML = `<p>A estação é: Primavera</p>`;
-    document.body.style.backgroundColor = "blue";
+    mostraResultado.innerHTML = `<p>A estação é: Primavera</p>`;
+    document.body.style.backgroundColor = "green";
 
     }
 }
---
+++ exercicio9.html/script.js
@@ -0,0 +1,15 @@
+let mostraResultado = document.querySelector(`#resultado`);
+let num1 = Number(window.prompt("digite o primeiro número: "));
+let num2 = Number(window.prompt("digite o segundo número: ")); 
+
+function digite() {
+    let num1 = window.prompt("Digite o primeiro número: ").toUpperCase()
+    let num2 = window.prompt("Digite o segundo número: ").toUpperCase()
+if (num1 > num2) {
+    alert(`o numero ${num1} é maior que ${num2}.`);
+if(num1 - num2 > 10)  {
+}
+}
+mostraResultado.innerHTML = `<h2>Acredite em si mesmo e você será imparável.</h2>`;
+
+}
